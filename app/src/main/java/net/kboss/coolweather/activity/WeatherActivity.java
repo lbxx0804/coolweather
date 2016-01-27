5f029dab4cdbd3bd5da88db1bf0eddd2b31bef76
lbxx0804 <lbxx0804@github.com>
2016-01-27 21:16:21 +0800

    部分完成天气显示功能

---
+++ app/src/main/java/net/kboss/coolweather/activity/WeatherActivity.java
@@ -0,0 +1,106 @@
+package net.kboss.coolweather.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import net.kboss.coolweather.R;
+
+/**
+ * Created by Administrator on 2016/1/27.
+ */
+public class WeatherActivity extends Activity {
+
+    private LinearLayout weatherInfoLayout;
+    /**
+     * 用于显示城市名
+     */
+    private TextView cityNameText;
+
+    /**
+     * 显示发布时间
+     */
+    private TextView publishText;
+
+    /**
+     * 用于显示天气描述信息
+     */
+    private TextView weatherDespText;
+
+    /**
+     * 用于显示气温1
+     */
+    private TextView temp1Text;
+    /**
+     * 用于显示气温2
+     */
+    private TextView temp2Text;
+    /**
+     * 用于显示当前日期
+     */
+    private TextView currentDateText;
+    /**
+     * 切换城市按钮
+     */
+    private Button switchCity;
+    /**
+     * 更新天气按钮
+     */
+    private Button refreshWeather;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.weather_layout);
+        weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+        cityNameText = (TextView) findViewById(R.id.city_name);
+        publishText = (TextView) findViewById(R.id.publish_text);
+        weatherDespText = (TextView) findViewById(R.id.weather_desp);
+        temp1Text = (TextView)findViewById(R.id.temp1);
+        temp2Text = (TextView) findViewById(R.id.temp2);
+        currentDateText = (TextView) findViewById(R.id.current_date);
+
+        String countyCode = getIntent().getStringExtra("county_code");
+        if (!TextUtils.isEmpty(countyCode)) {
+            // 有县级代号时就去查询天气
+            publishText.setText("同步中...");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherCode(countyCode);
+        } else {
+            // 没有县级代号时就直接显示本地天气
+            showWeather();
+        }
+    }
+
+    private  void queryWeatherCode(String countyCode){
+        String address = "http://www.weather.com.cn/data/list3/city" +
+                countyCode + ".xml";
+        queryFromServer(address, "countyCode");
+    }
+
+    /**
+     * 查询天气代号所对应的天气。
+     */
+    private void queryWeatherInfo(String weatherCode) {
+        String address = "http://www.weather.com.cn/data/cityinfo/" +
+                weatherCode + ".html";
+        queryFromServer(address, "weatherCode");
+    }
+
+    /**
+     * 根据传入的地址和类型去向服务器查询天气代号或者天气信息。
+     */
+    private void queryFromServer(final String address, final String type) {
+
+    }
+
+    private  void  showWeather(){
+
+    }
+}
--- app/src/main/java/net/kboss/coolweather/util/Utility.java
+++ app/src/main/java/net/kboss/coolweather/util/Utility.java
@@ -1,11 +1,21 @@
 package net.kboss.coolweather.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import net.kboss.coolweather.db.CoolWeatherDB;
 import net.kboss.coolweather.model.City;
 import net.kboss.coolweather.model.County;
 import net.kboss.coolweather.model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Scan on 2016-01-27.
@@ -74,6 +84,47 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 解析服务器返回的JSON数据，并将解析出的数据存储到本地。
+     */
+    public static void handleWeatherResponse(Context context, String response) {
+        /*{"weatherinfo":
+            {"city":"昆山","cityid":"101190404","temp1":"21℃","temp2":"9℃",
+                    "weather":"多云转小雨","img1":"d1.gif","img2":"n7.gif","ptime":"11:00"}
+        }
+        */
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesc = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesc,publishTime);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 将服务器返回的所有天气信息存储到SharedPreferences文件中。
+     */
+    public static void saveWeatherInfo(Context context, String cityName, String weatherCode, String temp1, String temp2, String weatherDesp, String publishTime){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_seleced",true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2",temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
     }
 
 }
